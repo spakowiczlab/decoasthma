2157c6217f3635ceffca41094c5f5245568f8a00
Dan Spakowicz <daniel.spakowicz@gmail.com>
2020-03-20 22:12:10 -0400

    Clean

--- data-processing/generate_exceRpt_lsf_sub.sh
+++ data-processing/generate_exceRpt_lsf_sub.sh
@@ -1,11 +1,9 @@
 # generate_exceRpt_lsf_sub_v5.sh
 # Dan Spakowicz
 # 15 Jan 2017
-# This script generates the submission scripts for all of the Chupp data with the dev version
-# of the pipeline that generates the rRNA tables
+# This script generates the submission scripts for all of the data
 
-cd /project/fas/gerstein/djs88/chupp/bulkCellRNAseq/fastq/
-OUTPATH="/gpfs/home/fas/gerstein/djs88/myscripts/projects/chas/exceRpt_submission/withrRNAtables/moreMem_nthreads20"
+OUTPATH=$DEF_DIR
 
 for D in `find Sample* -type d ` ; do
      echo '#BSUB -M 96000' >> $OUTPATH/${D}_exceRpt.sh
@@ -17,7 +15,7 @@ for D in `find Sample* -type d ` ; do
      echo "# Produced by generate_exceRpt_lsf_sub_v5.sh" >> $OUTPATH/${D}_exceRpt.sh
      echo "# Dan Spakowicz" >> $OUTPATH/${D}_exceRpt.sh
      echo "# 15 Jan 2017" >> $OUTPATH/${D}_exceRpt.sh
-     echo "# Submission file for bulkRNAseq data on lsf with exceRpt. For v3, there is more memory allocated than in v2 and doesn't delete old versions of files. For v4, a bigmem node with 1tb RAM queue was used and the memory allocated accordingly. This version uses the standard gerstein queue but does so with more threads, which hopefully will fix memory error exiting that occurred with v3." >> $OUTPATH/${D}_exceRpt.sh
+     echo "# Submission file for bulkRNAseq data on lsf with exceRpt. " >> $OUTPATH/${D}_exceRpt.sh
      echo "" >> $OUTPATH/${D}_exceRpt.sh
      echo "PIPELINE=/project/fas/gerstein/tg397/exceRpt_longRNA_dev_test_DS/new/exceRpt_longRNA_dev" >> $OUTPATH/${D}_exceRpt.sh
      echo "make -f \$PIPELINE \\" >> $OUTPATH/${D}_exceRpt.sh
